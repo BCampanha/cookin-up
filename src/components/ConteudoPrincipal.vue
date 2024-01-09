ab5bc7a544e2691478a5b95cc1dfc91189f180d2
Beatriz Campanha <bcampanha@usp.br>
2024-01-09 10:46:26 -0300

    criando tag estilizada para reutilizar

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,12 +1,13 @@
 <script lang="ts">
   import SelecionarIngredientes from "./SelecionarIngredientes.vue"
+import Tag from "./Tag.vue";
   export default {
     data() {
       return {
         ingredientes: ['Alho', 'Manteiga', 'Orégano']
       }
     },
-    components: { SelecionarIngredientes }
+    components: { SelecionarIngredientes, Tag }
   }
 </script>
 
@@ -18,8 +19,8 @@
       </span>
 
       <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
-          {{ ingrediente }}
+        <li v-for="ingrediente in ingredientes" :key="ingrediente">
+          <Tag :texto="ingrediente"/>
         </li>
       </ul>
 
@@ -58,18 +59,6 @@
   justify-content: center;
   gap: 1rem 1.5rem;
   flex-wrap: wrap;
-}
-
-.ingrediente {
-  display: inline-block;
-  border-radius: 0.5rem;
-  min-width: 4.25rem;
-  padding: 0.5rem;
-  text-align: center;
-    transition: 0.2s;
-    color: var(--creme, #FFFAF3);
-  background: var(--coral, #F0633C);
-  font-weight: 700;
 }
 
 .lista-vazia {
